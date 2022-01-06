ddfe0683a2ed8a9598a67226a1b7012e1b85c4c1
원혁 이 <qnwnendevel@gmail.com>
2022-01-06 19:26:39 +0900

    저장

---
+++ Moka/Bson.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class Bson
+    {
+        public static string GetID()
+        {
+            return ObjectId.GenerateNewId().ToString();
+        }
+    }
+}
